7d08518b5ba176e1845693568dfd93b5a29ec073
Luuk Romeijn <luukromeijn@hotmail.nl>
2022-04-08 12:39:25 +0200

    Creating database attempt

---
+++ database_test.py
@@ -0,0 +1,30 @@
+import mysql.connector
+from mysql.connector import Error
+import pandas as pd
+
+def create_server_connection(host_name, user_name, user_password):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password
+        )
+        print("MySQL Database connection successful")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+    return connection
+
+connection = create_server_connection("localhost", "root", "root")
+
+def create_database(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        print("Database created successfully")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+create_database_query = "CREATE DATABASE distances"
+create_database(connection, create_database_query)
