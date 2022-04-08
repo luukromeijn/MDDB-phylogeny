a5430b798c6f5fc5865e30631df60a83cbac41d6
C.G.A. Carton <c.g.a.carton@umail.leidenuniv.nl>
2022-04-08 15:38:32 +0200

    Mafft ran from python file

--- src/Read.py
+++ src/Read.py
@@ -1,5 +1,7 @@
 from Bio import Phylo
 from Bio import SeqIO
+from Bio.Align.Applications import MafftCommandline
+from Bio.Phylo.Applications import RaxmlCommandline
 from alfpy.utils import seqrecords, distmatrix
 from alfpy.utils.data import subsmat
 from alfpy import wmetric
@@ -30,6 +32,7 @@ ref_tree = Phylo.read('RAxML_bestTree.output_1.tre', 'newick')
 #         clade.clades = new_clade.clades
 #         clade.branch_length = new_clade.branch_length   
 # Phylo.draw(ref_tree)
+
 matrix = subsmat.get('blosum62')
 unknown = open('Unknown_ITS.fasta')
 unknown_records = seqrecords.read_fasta(unknown)
@@ -45,12 +48,27 @@ for x in range(1):
     for y in range(seq_records.count-1):
         distances.append((seq_records.id_list[y], dist.pairwise_distance(y, seq_records.count-1)))
     best_distances = sorted(distances, key = lambda i: i[1])[:5]
+    # print(best_distances)
     ancestor = ref_tree.common_ancestor(best_distances[0][0], best_distances[1][0], best_distances[2][0], best_distances[3][0], best_distances[4][0])
     child_names = []
     find_names(ancestor, child_names)
     print(child_names)
     write_to_fasta(child_names, seq_records)
-Phylo.draw(ref_tree)
+
+# TRYING TO GET MAFFT AND RAXML TO WORK FROM WITHIN PYTHON BY CALLING COMMANDLINE
+# mafft_exe = "mafft-win\mafft.bat"
+# in_file = "new_file.fasta"
+# mafft_cline = MafftCommandline(mafft_exe, input=in_file)
+# stdout, stderr = mafft_cline()
+# with open("aligned.fasta", "w") as handle:
+#     handle.write(stdout)
+# raxml_exe = "raxmlGUI.exe"
+# raxml_cline = RaxmlCommandline(raxml_exe, sequences="aligned.fasta", model="PROTCATWAG", name="new_tree")
+# output, error = raxml_cline()
+# with open("new_tree.newick", "w") as new_handle:
+#     new_handle.write(output)
+
+# Phylo.draw(ref_tree)
     # for distance in best_distances:
     #     for clade in ref_tree.find_clades(name=distance[0]):
     #         print(clade.name)
