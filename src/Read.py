5d61ea1b894e5b09a319912b03eec98c074247f6
C.G.A. Carton <c.g.a.carton@umail.leidenuniv.nl>
2022-05-03 15:50:49 +0200

    k-mer distances and gtrgamma model for raxml

--- src/Read.py
+++ src/Read.py
@@ -5,8 +5,12 @@ from Bio.Phylo.Applications import RaxmlCommandline
 from alfpy.utils import seqrecords
 from alfpy.utils.data import subsmat
 from alfpy import wmetric
+from alfpy import word_pattern
+from alfpy import word_vector
+from alfpy import word_distance
 import os
 import glob
+from tqdm import tqdm
 
 def find_names(ancestor, names):
     for clade in ancestor.clades:
@@ -24,6 +28,9 @@ def write_to_fasta(childs, records):
     new_file.close()
 
 ref_tree = Phylo.read('RAxML_bestTree.output_1.tre', 'newick')
+# test_tree = Phylo.read('new_ref_tree.newick', 'newick')
+# Phylo.draw(ref_tree)
+# Phylo.draw(test_tree)
 matrix = subsmat.get('blosum62')
 unknown = open('Unknown_ITS.fasta')
 unknown_records = seqrecords.read_fasta(unknown)
@@ -31,22 +38,28 @@ unknown.close()
 fh = open('Reference_ITS.fasta')
 seq_records = seqrecords.read_fasta(fh)
 fh.close()
-for x in range(unknown_records.count):
+for x in tqdm(range(unknown_records.count)):
     if unknown_records.seq_list[x] not in seq_records.seq_list:
         seq_records.add(unknown_records.id_list[x], unknown_records.seq_list[x])
-        dist = wmetric.Distance(seq_records, matrix)
+        # dist = wmetric.Distance(seq_records, matrix)
+        pattern = word_pattern.create(seq_records.seq_list, word_size=6)
+        counts = word_vector.Counts(seq_records.length_list, pattern)
+        dist = word_distance.Distance(counts, 'google')
         distances = []
         for y in range(seq_records.count-1):
             distances.append((seq_records.id_list[y], dist.pairwise_distance(y, seq_records.count-1)))
-        best_distances = sorted(distances, key = lambda i: i[1])[:3]
+        distances = sorted(distances, key = lambda i: i[1])
+        print(distances)
+        best_distances = distances[:3]
         # print(best_distances)
         ancestor = ref_tree.common_ancestor(best_distances[0][0], best_distances[1][0], best_distances[2][0]) #, best_distances[3][0], best_distances[4][0]
         child_names = []
         find_names(ancestor, child_names)
         # print(child_names)
         write_to_fasta(child_names, seq_records)
+        # Phylo.draw(ref_tree)
 
-        # MAFFT AND RAXML FROM WITHIN PYTHON BY CALLING COMMANDLINE
+        # TRYING TO GET MAFFT AND RAXML TO WORK FROM WITHIN PYTHON BY CALLING COMMANDLINE
         mafft_exe = "mafft-win\mafft.bat"
         in_file = "new_file.fasta"
         mafft_cline = MafftCommandline(mafft_exe, input=in_file)
@@ -54,12 +67,12 @@ for x in range(unknown_records.count):
         with open("aligned.fasta", "w") as handle:
             handle.write(stdout)
         raxml_exe = "raxmlHPC-PTHREADS-SSE3.exe"
-        raxml_cline = RaxmlCommandline(raxml_exe, sequences="aligned.fasta", model="PROTCATWAG", name="new_tree")
+        raxml_cline = RaxmlCommandline(raxml_exe, sequences="aligned.fasta", model="GTRGAMMA", name="new_tree")
         output, error = raxml_cline()
         new_subtree = Phylo.read('RAxML_bestTree.new_tree', 'newick')
         new_clade = new_subtree.root
         ancestor.clades = new_clade.clades
-        
+        # Phylo.draw(ref_tree)
         # DELETE ALL RAXML FILES
         files = glob.glob('*new_tree')
         for f in files:
@@ -69,14 +82,14 @@ for x in range(unknown_records.count):
 Phylo.write(ref_tree, 'new_ref_tree.newick', 'newick')
 Phylo.draw(ref_tree)
 
-# Phylo.draw(ref_tree)
-    # for distance in best_distances:
-    #     for clade in ref_tree.find_clades(name=distance[0]):
-    #         print(clade.name)
-    # for clade in ref_tree.find_clades(name=best_distances[0][0]):
-    #     print(clade.clades)
+# # Phylo.draw(ref_tree)
+#     # for distance in best_distances:
+#     #     for clade in ref_tree.find_clades(name=distance[0]):
+#     #         print(clade.name)
+#     # for clade in ref_tree.find_clades(name=best_distances[0][0]):
+#     #     print(clade.clades)
 
-# data = SeqIO.index('Reference_ITS.fasta', 'fasta')
-# for line in data:
-#     print(line, data[line].seq[:10] + '...' + data[line].seq[-10:])
-#     # print(data[line].seq)
+# # data = SeqIO.index('Reference_ITS.fasta', 'fasta')
+# # for line in data:
+# #     print(line, data[line].seq[:10] + '...' + data[line].seq[-10:])
+# #     # print(data[line].seq)
